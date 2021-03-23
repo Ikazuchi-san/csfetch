5af92f4ed559c7425125887138a11e0dc7848fd9
Paulo de Tarso Nobrega Rodrigues <paulo.tarso.nr77@gmail.com>
2021-03-23 19:08:46 -0300

    Add basic project skeleton

---
+++ sitefetch/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace sitefetch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
